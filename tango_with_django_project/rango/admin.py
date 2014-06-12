2e8a178cb87c3434fbc556d87ae1e0579de84146
Kevin Hurley <kevin@hurleys.org.uk>
2014-06-12 16:26:20 +0100

    Worked as far as section 5.7 of the tutorial

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+import rango.models
 # Register your models here.
+admin.site.register(rango.models.Category)
+admin.site.register(rango.models.Page)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -56,10 +56,11 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+DATABASE_PATH = os.path.join(BASE_DIR, 'rango.db')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': DATABASE_PATH,
     }
 }
 
