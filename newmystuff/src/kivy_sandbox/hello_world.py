22ec28b590f62f837469d351556808c44774b27e
Kevin Hurley <kevin@hurleys.org.uk>
2014-06-12 12:14:13 +0100

    Initial files added

---
+++ newmystuff/src/kivy_sandbox/hello_world.py
@@ -0,0 +1,14 @@
+'''
+Created on 10 Jun 2014
+
+@author: khurley
+'''
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+    
+if __name__ == '__main__':
+    TestApp().run()
