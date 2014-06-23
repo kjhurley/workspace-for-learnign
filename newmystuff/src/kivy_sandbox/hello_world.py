645b168697fec9b1f5f02462c9c5ff8c0df63764
Kevin Hurley <kevin@hurleys.org.uk>
2014-06-23 14:25:45 +0100

    Added some files that were missed from last work on django tutorial

--- newmystuff/src/kivy_sandbox/hello_world.py
+++ newmystuff/src/kivy_sandbox/hello_world.py
@@ -5,10 +5,17 @@ Created on 10 Jun 2014
 '''
 from kivy.app import App
 from kivy.uix.button import Button
+import kivy.uix.image
+
+class ShowImage(kivy.uix.image.Image):
+    pass
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello World')
+        if True:
+            return ShowImage(source="rango.jpg",pos=(0,0),size=(256,256))
+        else:
+#return Button(text='Hello World')
     
 if __name__ == '__main__':
     TestApp().run()
