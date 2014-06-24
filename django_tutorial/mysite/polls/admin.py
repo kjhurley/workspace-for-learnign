e3ccb95c515a807c9455fd2c7d9b52ea66432915
Kevin Hurley <kevin@hurleys.org.uk>
2014-06-24 14:15:00 +0100

    Finished all of django tutorial part 2

--- django_tutorial/mysite/polls/admin.py
+++ django_tutorial/mysite/polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+import polls.models
 # Register your models here.
+
+# use tabular inline baseclass to get a nice uncluttered set of input fields
+class ChoiceInline(admin.TabularInline):
+    model = polls.models.Choice
+    extra = 3 # make room for 3 choices on the admin page
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets =[
+                (None,{'fields':['question_text']}),
+                ('Data information',{'fields':['pub_date'],'classes':['collapse']}),
+                ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(polls.models.Question, QuestionAdmin)
+#admin.site.register(polls.models.Choice)
+
+
+
+
--- newmystuff/src/guide/single_channel_listing.py
+++ newmystuff/src/guide/single_channel_listing.py
@@ -1,9 +1,8 @@
 from kivy.app import App
-import kivy.uix.widget
 from kivy.uix.button import Button
 import kivy.uix.label 
 import kivy.uix.gridlayout
-import kivy.uix.boxlayout
+import kivy.uix.relativelayout
 
 """
 top line - channel icon in top left, current time in top right
@@ -25,53 +24,29 @@ alternative click to record it (right mouse button or something)
 """
 
 def generate_show_details_label(shows, show_id):
-    """ format the show details to appear in the details box
-    
-    Whichever show is currently selected, a box with details is displayed near the
-    top of the app showing details of that show incuding start time, name of the programme,
-    a detailed description of the show etc
-    
-    """
-    print shows[show_id]
-    return "{starttime:<4}: {name: >40}\n{details}".format(**shows[show_id])
+    return "%(start time)s --- %(name)s\n%(details)s"%shows[show_id]
 
 def select_show_event(obj):
-    """ when a show is selected, update the details box """
     new_text=generate_show_details_label(obj.parent.parent.shows,obj.show_id)
     print new_text
     obj.parent.parent.show_details_label.text=new_text
 
-class SingleChannelGuide(kivy.uix.widget.Widget):
-    pass
-
-class SingleChannelGuideApp(App):
+class TestApp(App):
     def build(self):
-        return SingleChannelGuide()
-
-"""    def build(self):
         
         listing=[("0900","First show","The first show in the morning"),
                  ("0930","Second show","Something else to watch"),
                  ("1030","Movie 1","Best film of all time"),
-                 ("1300","News at lunch","Did you miss it this yesterday?"),
-                 ("1400","Movie 2","A real tear-jerker"),
-                 ("1700","Train reks","For the kids")]
+                 ("1300","News at lunch","Did you miss it this yesterday?")]
         
-        outer_layout=kivy.uix.boxlayout.BoxLayout(orientation='vertical')
-        selected_program_box=kivy.uix.gridlayout.GridLayout(rows=2)
-        channel_box=kivy.uix.anchorlayout.AnchorLayout(anchor_x='left',anchor_y='top')
-        channel_box.add_widget(kivy.uix.label.Label(text="BBC One"))
-        selected_program_box.add_widget(channel_box)
-        
+        outer_layout=kivy.uix.gridlayout.GridLayout(rows=4)
         outer_layout.create_property("shows")  
         outer_layout.shows={}
-        [outer_layout.shows.update({show_id:{'name':entry[1],'starttime':entry[0],'details':entry[2]}}) for show_id,entry in enumerate(listing)]
+        [outer_layout.shows.update({show_id:{'name':entry[0],'start time':entry[1],'details':entry[2]}}) for show_id,entry in enumerate(listing)]
          
         outer_layout.create_property("show_details_label")
-        outer_layout.show_details_label=kivy.uix.label.Label(text=generate_show_details_label(outer_layout.shows,0),size_hint=(1.0,0.2))
-        
+        outer_layout.show_details_label=kivy.uix.label.Label(text=generate_show_details_label(outer_layout.shows,0))
         outer_layout.add_widget(outer_layout.show_details_label)
-        
         middle_layout=kivy.uix.gridlayout.GridLayout(cols=1,row_force_default=True, row_default_height=40)
         for i,l in enumerate(listing):
             b=Button(text="%4s   %s"%(l[0],l[1]))
@@ -82,7 +57,6 @@ class SingleChannelGuideApp(App):
             
         outer_layout.add_widget(middle_layout)
         return outer_layout
-"""
     
 if __name__ == '__main__':
-    SingleChannelGuideApp().run()
+    TestApp().run()
