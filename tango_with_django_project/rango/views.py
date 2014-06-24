e526aa2bc5d19c60847db24024c5bea5a5da0eb5
Kevin Hurley <kevin@hurleys.org.uk>
2014-06-24 15:32:22 +0100

    Finished working through tango with django chapter 6 except the exercises.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,21 @@
 # Create your views here.
 import django.template
 import django.shortcuts
+import rango.models
 
 def index(request):
     context=django.template.RequestContext(request)
-    context_dict={'boldmessage':"I am a bold font from the context"}
     
+    # order by number of likes in descending order. retrieve at most the top five
+    category_list=rango.models.Category.objects.order_by('-likes')[:5]
+    
+    # put this list in the context dict used to render the page
+    context_dict={'categories':category_list}
+    
+    for category in category_list:
+        category.url = category.name.replace(' ','_')
+    
+    # render the response
     return django.shortcuts.render_to_response("rango/index.html",context_dict, context)
 
 def about(request):
@@ -15,3 +25,22 @@ def about(request):
                   'front_page_link':"/rango"}
     
     return django.shortcuts.render_to_response("rango/about.html",context_dict, context)
+
+def category(request, category_name_url):
+    context = django.template.RequestContext(request)
+    
+    category_name = category_name_url.replace('_',' ')
+    context_dict = {'category_name': category_name}
+    
+    try:
+        # will throw DoesNotExist if no match
+        category = rango.models.Category.objects.get(name=category_name)
+        
+        pages = rango.models.Page.objects.filter(category=category)
+        
+        context_dict['pages']=pages
+        context_dict['category']=category
+    except rango.models.Category.DoesNotExist:
+        # template will report 'no categories found' if passed an empty list anyway
+        pass
+    return django.shortcuts.render_to_response('rango/category.html',context_dict, context)
