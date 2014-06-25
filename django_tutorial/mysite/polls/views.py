39595f4a3656e5b6c9ea1674ecc913ac5575b45d
Kevin Hurley <kevin@hurleys.org.uk>
2014-06-25 10:29:25 +0100

    Some new files as part of django tutorial part 3

--- django_tutorial/mysite/polls/views.py
+++ django_tutorial/mysite/polls/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
-
+import django.shortcuts
 # Create your views here.
-
-from django.http import HttpResponse
+from polls.models import Question
+import django.http
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Question.objects.order_by("-pub_date")[:5]
+    context={'latest_poll_list':latest_poll_list}
+    return django.shortcuts.render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    question = django.shortcuts.get_object_or_404(Question,pk=poll_id)
+    return django.shortcuts.render(request, 'polls/detail.html', {'question':question})
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    return django.http.HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    return django.http.HttpResponse("You're voting on poll %s." % poll_id)
