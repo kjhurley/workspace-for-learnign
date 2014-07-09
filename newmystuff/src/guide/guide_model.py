d9066f83dce74adafc07750307605c29a381c57a
Kevin Hurley <kevin@hurleys.org.uk>
2014-07-09 10:05:21 +0100

    Improvements to the guide_model module and associated unit tests

--- newmystuff/src/guide/guide_model.py
+++ newmystuff/src/guide/guide_model.py
@@ -13,21 +13,94 @@ although the keys are actually 'channel id' and the times are seconds since 1970
 
 """
 
+class Programme:
+    """ holds information on a single broadcast entity
+    
+      start_time - when the broadcast commenced
+      title - what it's called
+      details - whatever description was supplied by the guide
+      """
+    def __init__(self, start_time, title, details):
+        self.start=start_time
+        self.title=title
+        self.details=details  
+
+    @classmethod
+    def from_htsp(cls,event_msg):
+        return cls(start_time=event_msg["start"], title=event_msg["title"], details=event_msg["description"])
+
+
+class Channel:
+    """ knows about channels - their names, channel id etc 
+    
+        channel_id - a unique number for the channel used internally
+        channel_name - what the broadcaster calls the channel e.g. BBC One
+        channel_number - e.g. freeview channel
+        
+        channel also presents an iterator interface for iterating over the listing
+    """
+    def __init__(self, channel_id, name, number):
+        self.cid = channel_id
+        self.name=name
+        self.number=number
+        self.listing=[]
+
+    def htsp_add_programme_event(self, event_msg):
+        self.listing+=[Programme.from_htsp(event_msg)]
+    @classmethod
+    def htsp_add_channel_event(klass, event_msg):
+        return klass(channel_id=event_msg['channelId'], name=event_msg['channelName'], number=event_msg['channelNumber'])
+    
+    def add_programme(self, programme):
+        """ insert programme into the listing in chronological order """
+        self.listing+=[programme]
+        if len(self.listing)>1 and not programme.start>self.listing[-2].start:
+            self.listing.sort(lambda x,y: cmp(x.start,y.start))                    
 
 class Guide:
     """ a guide holds information about whats on - it consists of channels """
     def __init__(self):
         self._channelInfo={}
+        self._channels={} # key by channelId
         
     def add_a_channel(self, new_channel_info):
         """ add a channel using info from tv head end """
-        self._channelInfo[new_channel_info['channelId']]=new_channel_info['channelName']
+        new_id=new_channel_info['channelId']
+        self._channelInfo[new_id]=Channel.htsp_add_channel_event(new_channel_info)
+        self._channels[new_id]=[]
 
-    def channelName(self, channel_id):
+    def channel(self, channel_id):
         """ identify the name of the given channel """
         return self._channelInfo[channel_id]
     
     def add_a_programme(self, new_programme_info):
         """ add a single show (on a given channel) """
+        channel_id=new_programme_info['channelId']
+        p=Programme.from_htsp(new_programme_info)
+        self._channelInfo[channel_id].add_programme(p)
+        self._channels[channel_id]+=[]
         
-    def listing(self, channel_id):
+        
+    def listing(self, **kwargs):
+        """ listing can be searched using channel name or channel id or channel number 
+        
+        what's returned is a list of programmes on that channel.
+        """
+        if "channel_id" in kwargs:
+            return self._channelInfo[kwargs['channel_id']].listing
+        elif "channel_name" in kwargs:
+            for n in self._channelInfo:
+                if self._channelInfo[n].name==kwargs['channel_name']:
+                    return self._channelInfo[n].listing
+            raise IndexError("name %s not found in %s"%(kwargs['channel_name'], self._channelInfo))
+                    
+            return self._channels[kwargs['channel_name']]
+        elif "channel_number" in kwargs:
+            for n in self._channelInfo:
+                if self._channelInfo[n].number==kwargs['channel_number']:
+                    return self._channelInfo[n].listing
+            raise IndexError("name %s not found in %s"%(kwargs['channel_name'], self._channelInfo))
+                    
+            return self._channels[kwargs['name']]
+            
+    
--- newmystuff/src/guide/test_guide_model.py
+++ newmystuff/src/guide/test_guide_model.py
@@ -1,21 +1,141 @@
 import guide_model
 import unittest
 
-class TestCase(unittest.TestCase):
+class GuideTests(unittest.TestCase):
     def test_add_a_channel(self):
         input_1_from_htsp={'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'}
         g=guide_model.Guide()
         g.add_a_channel(input_1_from_htsp)
-        self.assertEqual(g.channelName(7), "4Music")
+        self.assertEqual(g.channel(7).name, "4Music")
+        
+    def test_two_channels(self):
+        input_1_from_htsp={'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'}
+        g=guide_model.Guide()
+        g.add_a_channel(input_1_from_htsp)
+        self.assertEqual(g.channel(7).name, "4Music")
         input_2_from_htsp={'eventId': 116108, 'tags': [1, 2, 3], 'nextEventId': 116110, 'channelId': 8, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': 'More4', 'method': 'channelAdd'}
         g.add_a_channel(input_2_from_htsp)
-        self.assertEqual(g.channelName(8), "More4")
+        self.assertEqual(g.channel(8).name, "More4")
 
     def test_add_a_programme(self):
         input_from_htsp={'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'}
         g=guide_model.Guide()
         g.add_a_channel(input_from_htsp)
-        more_input_from_htsp={'eventId': 116107, 'serieslinkId': 76020, 'contentType': 96, 'description': "It's the pop world cup and today's match is the quarter final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 'nextEventId': 116109, 'channelId': 7, 'stop': 1404492000, 'episodeId': 116108, 'start': 1404489600, 'method': 'eventAdd'}
+        more_input_from_htsp={'eventId': 116107, 'serieslinkId': 76020, 'contentType': 96, 'description': "It's the pop world cup and today's match is the quarter final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 'nextEventId': 116109, 
+                              'channelId': 7, 'stop': 1404492000, 'episodeId': 116108, 'start': 1404489600, 'method': 'eventAdd'}
         g.add_a_programme(more_input_from_htsp)
-        self.assertEqual(g.listing(8)[0]['title'],'Pop World Cup: Rihanna v Lady Gaga')
+        self.assertEqual(g.channel(7).listing[0].title,'Pop World Cup: Rihanna v Lady Gaga')
+        self.assertEqual(g.listing(channel_id=7)[0].start,1404489600)
         
+    def test_get_a_programme_from_listing_by_freeview_channel(self):
+        input_from_htsp={'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'}
+        g=guide_model.Guide()
+        g.add_a_channel(input_from_htsp)
+        more_input_from_htsp={'eventId': 116107, 'serieslinkId': 76020, 'contentType': 96, 'description': "It's the pop world cup and today's match is the quarter final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 'nextEventId': 116109, 
+                              'channelId': 7, 'stop': 1404492000, 'episodeId': 116108, 'start': 1404489600, 'method': 'eventAdd'}
+        g.add_a_programme(more_input_from_htsp)
+        self.assertEqual(g.channel(channel_id=7).listing[0].title,'Pop World Cup: Rihanna v Lady Gaga')
+        self.assertEqual(g.listing(channel_number=18)[0].start,1404489600)
+        
+    def test_add_two_programmes_on_same_channel(self):
+        input_from_htsp={'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'}
+        g=guide_model.Guide()
+        g.add_a_channel(input_from_htsp)
+        more_input_from_htsp={'eventId': 116107, 'serieslinkId': 76020, 'contentType': 96, 'description': "It's the pop world cup and today's match is the quarter final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 'nextEventId': 116109, 
+                              'channelId': 7, 'stop': 1404492000, 'episodeId': 116108, 'start': 1404489600, 'method': 'eventAdd'}
+        g.add_a_programme(more_input_from_htsp)
+        self.assertEqual(g.listing(channel_id=7)[0].title,'Pop World Cup: Rihanna v Lady Gaga')
+        another_show_from_htsp={'eventId': 116108, 'serieslinkId': 76001, 'contentType': 96,
+                                 'description': "It isn't the pop world cup and tomorrrow's match is the final between Madonna and Bob Dylan. This could go either way. It's time to decide which stand you'll be seated in!", 
+                                 'title': 'Pop World Cup: Madonna v Bob Dylan', 'nextEventId': 116109, 
+                                 'channelId': 7, 'stop': 1404498000, 'episodeId': 116200, 'start': 1404489700, 'method': 'eventAdd'}
+        g.add_a_programme(another_show_from_htsp)
+        self.assertEqual(g.listing(channel_id=7)[1].title,'Pop World Cup: Madonna v Bob Dylan')
+        self.assertEqual(g.listing(channel_id=7)[1].start,1404489700)
+        
+    def test_add_two_programmes_on_two_different_channels(self):
+        input_1_from_htsp={'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'}
+        g=guide_model.Guide()
+        g.add_a_channel(input_1_from_htsp)
+        self.assertEqual(g.channel(7).name, "4Music")
+        input_2_from_htsp={'eventId': 116108, 'tags': [1, 2, 3], 'nextEventId': 116110, 'channelId': 8, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': 'More4', 'method': 'channelAdd'}
+        g.add_a_channel(input_2_from_htsp)
+        self.assertEqual(g.channel(8).name, "More4")
+        more_input_from_htsp={'eventId': 116107, 'serieslinkId': 76020, 'contentType': 96, 
+                              'description': "It's the pop world cup and today's match is the quarter final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 
+                              'nextEventId': 116109, 'channelId': 7, 'stop': 1404492000, 
+                              'episodeId': 116108, 'start': 1404489600, 'method': 'eventAdd'}
+        g.add_a_programme(more_input_from_htsp)
+        # first show is on channel id 7
+        self.assertEqual(g.listing(channel_id=7)[0].title,'Pop World Cup: Rihanna v Lady Gaga')
+        another_show_from_htsp={'eventId': 116108, 'serieslinkId': 76001, 'contentType': 96,
+                                 'description': "It isn't the pop world cup and tomorrrow's match is the final between Madonna and Bob Dylan. This could go either way. It's time to decide which stand you'll be seated in!", 
+                                 'title': 'Pop World Cup: Madonna v Bob Dylan', 'nextEventId': 116109, 
+                                 'channelId': 8, 'stop': 1404498000, 'episodeId': 116200, 'start': 1404489700, 'method': 'eventAdd'}
+        g.add_a_programme(another_show_from_htsp)
+        # second one is on channel id 8
+        self.assertEqual(g.listing(channel_name="More4")[0].title,'Pop World Cup: Madonna v Bob Dylan')
+        
+        self.assertEqual(g.channel(8).listing[0].title,
+                         g.listing(channel_name="More4")[0].title)
+
+    def test_add_four_programmes_on_two_different_channels_in_chronological_order(self):
+        
+        g=guide_model.Guide()
+        g.add_a_channel({'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 7, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': '4Music', 'method': 'channelAdd'})
+        self.assertEqual(g.channel(7).name, "4Music")
+        
+        
+        g.add_a_programme({'eventId': 116107, 'serieslinkId': 76020, 'contentType': 96, 
+                              'description': "It's the pop world cup and today's match is the quarter final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 
+                              'nextEventId': 116109, 'channelId': 7, 'stop': 1404492000, 
+                              'episodeId': 116108, 'start': 1404489600, 'method': 'eventAdd'})
+        g.add_a_programme({'eventId': 116114, 'serieslinkId': 76020, 'contentType': 96, 
+                              'description': "It's the pop world cup and today's match is the semi final between Rihanna and Lady Gaga. This could go either way. It's time to decide which stand you'll be seated in!", 'title': 'Pop World Cup: Rihanna v Lady Gaga', 
+                              'nextEventId': 116115, 'channelId': 7, 'stop': 1404493200, 
+                              'episodeId': 116110, 'start': 1404492000, 'method': 'eventAdd'})
+
+        # first show is on channel id 7
+        self.assertEqual(g.listing(channel_id=7)[0].title,'Pop World Cup: Rihanna v Lady Gaga')
+        
+        g.add_a_channel({'eventId': 116107, 'tags': [1, 2, 3], 'nextEventId': 116109, 'channelId': 8, 'channelNumber': 18, 'services': [{'type': 'SDTV', 'name': 'DiBcom 7000PC/West: 722,000 kHz/4Music'}], 'channelName': 'More4', 'method': 'channelAdd'})
+        self.assertEqual(g.channel(8).name, "More4")
+ 
+        g.add_a_programme({'eventId': 116108, 'serieslinkId': 76001, 'contentType': 96,
+                                 'description': "It isn't the pop world cup and tomorrrow's match is the final between Madonna and Bob Dylan. This could go either way. It's time to decide which stand you'll be seated in!", 
+                                 'title': 'Pop World Cup: Madonna v Bob Dylan', 'nextEventId': 116109, 
+                                 'channelId': 8, 'stop': 1404498000, 'episodeId': 116200, 'start': 1404489700, 'method': 'eventAdd'})
+        g.add_a_programme({'eventId': 116116, 'serieslinkId': 76001, 'contentType': 96,
+                                 'description': "It isn't the pop world cup and tomorrrow's match is the final between Madonna and Bob Dylan. This could go either way. It's time to decide which stand you'll be seated in!", 
+                                 'title': 'Pop World Cup: Barry White v Rod Stewart', 'nextEventId': 116117, 
+                                 'channelId': 8, 'stop': 1404500000, 'episodeId': 116200, 'start': 1404499000, 'method': 'eventAdd'})
+        # second one is on channel id 8
+        self.assertEqual(g.listing(channel_name="More4")[0].title,'Pop World Cup: Madonna v Bob Dylan')
+        self.assertEqual(g.listing(channel_name="More4")[1].title,'Pop World Cup: Barry White v Rod Stewart')
+        
+        self.assertEqual(g.channel(8).listing[0].title,
+                         g.listing(channel_name="More4")[0].title)
+        
+class ChannelTest(unittest.TestCase):
+    def test_programmes_are_added_in_chronological_order(self):
+        c=guide_model.Channel(8,name="More4",number=18 )
+        # args are start time, title, details ...
+        p0=guide_model.Programme(0,"first show","nonsense")
+        p1=guide_model.Programme(1,"second show","more nonsense")
+        p2=guide_model.Programme(2,"third show","the worst nonsense")
+        p3=guide_model.Programme(3,"fourth show","excessive nonsense")
+        c.add_programme(p1)
+        c.add_programme(p3)
+        c.add_programme(p2)
+        c.add_programme(p0)
+                
+        self.assertEqual(c.listing[0].title,"first show")
+        self.assertEqual(c.listing[1].title,"second show")
+        self.assertEqual(c.listing[2].title,"third show")
+        self.assertEqual(c.listing[3].title,"fourth show")
+
+class ChannelInfoTests(unittest.TestCase):
+    def test_create_a_channel(self):
+        c=guide_model.Channel(8,name="More4",number=18 )
+        self.assertEqual(c.name, "More4")
+        self.assertEqual(c.number, 18)
